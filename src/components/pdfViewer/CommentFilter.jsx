215b6bf45ac04ea2b71b70cf4a87646f365bd06c
Parveen Chaudhary <parveen@csgroupchd.com>
2024-12-09 16:55:53 +0530

    Made the changes in the code

--- src/components/pdfViewer/CommentFilter.jsx
+++ src/components/pdfViewer/CommentFilter.jsx
@@ -2,8 +2,7 @@ const CommentFilter = ({ handleFilterRecords }) => {
     return (
         <div className='flex border-b border-[#f8f8f8] my-4 gap-8 items-center px-[17px]'>
             <select
-                className='custom-select-box bg-white p-[8px_18px] ring-0 outline-[0] appearance-none rounded-md cursor-pointer'
-                name=""
+                className='custom-select-box bg-white p-[8px_18px] ring-0 outline-[0] appearance-none rounded-md cursor-pointer' 
                 id=""
                 onChange={handleFilterRecords}
             >
